cd395d2935a9b838d84a3afcfed878927acf7c83
bstafford2017 <bstafford2017@gmail.com>
2020-01-28 16:28:30 -0600

    remove fade dependency

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Item from './Item'
-import { Fade, Row, Alert } from 'reactstrap';
+import { Row, Alert } from 'reactstrap';
 
 export class ItemList extends Component {
   render() {
