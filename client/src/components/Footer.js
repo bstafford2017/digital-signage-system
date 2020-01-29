4a111f7c0bac1c60d15d8cbba990ea404aa27f98
bstafford2017 <bstafford2017@gmail.com>
2020-01-28 22:10:00 -0600

    sticky footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,11 +4,11 @@ export class Footer extends Component {
   
   render() {
     return (
-      <div className="bg-dark relative-bottom text-center text-white" 
-        style={{height: '175px', width: '100%', padding: '40px', lineHeight: '1'}}>
+      <div className="bg-dark fixed-bottom text-center text-white" 
+        style={{height: '100px', width: '100%', padding: '10px', lineHeight: '1'}}>
         <p>Benjamin Stafford &copy; 2020</p>
         <p>School of Electrical Engineering and Computer Science</p>
-        <p>Univeristy of North Dakota</p>
+        <p>University of North Dakota</p>
       </div>
     )
   }
